05e57c885a6b9b248f172530d80d0227a738ea8f
Millad Dagdoni <milladd@gmail.com>
2020-01-19 15:34:38 +0100

    Update traffic_lights_basic.py
    Fixed naming and cleaned comments

--- traffic_lights_basic.py
+++ traffic_lights_basic.py
@@ -2,14 +2,12 @@
 # Dagdoni.com
 import numpy as np
 
-np.random.seed(1)
-
 #Init stuff
 alpha = 0.2  #how fast do you want to move the slope?
+np.random.seed(1)
 w = np.random.random((3,3)) - 1 #weights
 x = np.array([[1,0,0],[0,1,0],[0,0,1]]) #input
 y = np.array([1,1,0]).T #Output Wanted
-
 
 #learn
 for step in range(20):
@@ -20,11 +18,10 @@ for step in range(20):
         error += np.sum((layer_1_pred - y[i:i+1]) ** 2)
         derivant = alpha * (layer_0_x.T.dot(layer_1_pred - y[i:i+1]))
         w -= derivant
-        print("ERROR: " +  str(error) + "")
+        print("ERROR: " +  str(error))
 
-
-#Predict Test
+#Predict
 lightsIseeArray = np.array([1,0,0])
-shouldIdriveSum = np.sum(np.dot(lightsIseeArray, w))
-shouldIdrive = shouldIdriveSum > 0
+shouldIdriveSumResult = np.sum(np.dot(lightsIseeArray, w))
+shouldIdrive = shouldIdriveSumResult > 0
 print("Should you drive now? " + str(shouldIdrive))
