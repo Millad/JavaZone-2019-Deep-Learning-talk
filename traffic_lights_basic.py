9d6082e3f003dcaf7c21fa4a73c40271a485945f
Millad Dagdoni <milladd@gmail.com>
2020-12-01 02:26:52 +0100

    Update traffic_lights_basic.py
    Simlified and cleaned up variables for error and predictions

--- traffic_lights_basic.py
+++ traffic_lights_basic.py
@@ -4,24 +4,25 @@ import numpy as np
 
 #Init stuff
 alpha = 0.2  #how fast do you want to move the slope?
-np.random.seed(1)
+
 w = np.random.random((3,3)) - 1 #weights
 x = np.array([[1,0,0],[0,1,0],[0,0,1]]) #input
 y = np.array([1,1,0]).T #Output Wanted
+error = 0
 
 #learn
-for step in range(20):
-    error = 0
-    for i in range(len(x)):
-        layer_0_x = x[i:i+1]
-        layer_1_pred =(np.dot(layer_0_x, w))
-        error += np.sum((layer_1_pred - y[i:i+1]) ** 2)
-        derivant = alpha * (layer_0_x.T.dot(layer_1_pred - y[i:i+1]))
-        w -= derivant
-        print("ERROR: " +  str(error))
+if __name__ == "__main__":
+    for step in range(20):
+        for i in range(len(x)):
+            layer_0_x = x[i:i+1]
+            layer_1_pred =(np.dot(layer_0_x, w))
+            error = np.sum((layer_1_pred - y[i:i+1]) ** 2)
+            derivant = alpha * (layer_0_x.T.dot(layer_1_pred - y[i:i+1]))
+            w -= derivant
+            print("ERROR: " +  str(error))
 
-#Predict
-lightsIseeArray = np.array([1,0,0])
-shouldIdriveSumResult = np.sum(np.dot(lightsIseeArray, w))
-shouldIdrive = shouldIdriveSumResult > 0
-print("Should you drive now? " + str(shouldIdrive))
+    #Predict
+    greenLightsIseeArray = np.array([1,0,0])
+    shouldIdriveSumResult = np.sum(np.dot(greenLightsIseeArray, w))
+    shouldIdrive = shouldIdriveSumResult > 0
+    print("Should you drive now? " + str(shouldIdrive))
