fa56100ad0c1a5cc81dd8810889795bb5f81983a
Millad Dagdoni <milladd@gmail.com>
2025-08-07 21:10:08 +0200

    Update traffic_lights.py
    removed main method and simplified code a bit including easier print statements

--- traffic_lights.py
+++ traffic_lights.py
@@ -9,18 +9,17 @@ x = np.array([[1,0,0],[0,1,0],[0,0,1]]) #input
 y = np.array([1,1,0]).T #Output Wanted
 
 #learn
-if __name__ == "__main__":
-    for step in range(2):
-        for i in range(len(x)):
-            layer_0_x = x[i:i+1]
-            layer_1_pred =(np.dot(layer_0_x, w))
-            error = np.sum((layer_1_pred - y[i]) ** 2)
-            derivant = alpha * (layer_0_x.T.dot(layer_1_pred - y[i]))
-            w -= derivant
-            print("ERROR: " +  str(error))
+for step in range(2):
+    for i in range(len(x)):
+        layer_0_x = x[i:i+1]
+        layer_1_pred =(np.dot(layer_0_x, w))
+        error = np.sum((layer_1_pred - y[i]) ** 2)
+        derivant = alpha * (layer_0_x.T.dot(layer_1_pred - y[i]))
+        w -= derivant
+        print("ERROR: ",error)
 
-    #Predict
-    greenLightsIseeArray = np.array([1,0,0])
-    shouldIdriveSumResult = np.sum(np.dot(greenLightsIseeArray, w))
-    shouldIdrive = shouldIdriveSumResult > 0
-    print("Should you drive now? " + str(shouldIdrive))
+#Predict
+greenLightsIseeArray = np.array([1,0,0])
+shouldIdriveSumResult = np.sum(np.dot(greenLightsIseeArray, w))
+shouldIdrive = shouldIdriveSumResult > 0
+print("Should you drive now? ", shouldIdrive)
